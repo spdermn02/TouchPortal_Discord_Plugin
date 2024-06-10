f20418364b561335ebf7fec0769fcd8a72eaaa2f
Gitago <76603653+gitagogaming@users.noreply.github.com>
2024-06-10 03:13:33 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,16 @@
-module.exports = {
-    discordWin32: "Discord.exe"
-}
+const USERSTATES = [
+    { id: 'Speaking', title: 'Status: isSpeaking', value: 'Off' },
+    { id: 'id', title: 'ID', value: '' },
+    { id: 'nick', title: 'Nickname', value: '' },
+    { id: 'mute', title: 'Status: Mute', value: 'Off' },
+    { id: 'self_mute', title: 'Status: Self Mute', value: 'Off' },
+    { id: 'self_deaf', title: 'Status: Self Deaf', value: 'Off' },
+    { id: 'deaf', title: 'Status: Deafen', value: 'Off' },
+    { id: 'volume', title: 'Volume', value: '0' },
+    { id: 'avatar', title: 'Avatar', value: '' }
+  ];
+  
+  module.exports = {
+    discordWin32: "Discord.exe",
+    USERSTATES
+  };
