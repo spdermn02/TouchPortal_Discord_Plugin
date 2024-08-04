25899d03ebc4a995177d14264b1cd33861004e7b
Gitago <76603653+gitagogaming@users.noreply.github.com>
2024-08-04 00:41:09 -0400

    Update notificationHandler.js
    # notificationhandler
    - cleanup, switch statement,

--- src/handlers/discord/notificationHandler.js
+++ src/handlers/discord/notificationHandler.js
@@ -4,13 +4,13 @@ const {logIt, imageToBase64} = require("../../utils/helpers.js");
 class NotificationHandler {
   
   constructor(TPClient, DG) {
-    this.DG = DG;
     this.TPClient = TPClient;
+    this.DG = DG;
   }
   async onNotification(data) {
     // When getting DMs, or Tagged in a message.. can give  you details as to where.. so could set up a button to take to channel where tagged technically..
     // Select channel action would need a custom input for channel ID
-    logIt("INFO", "Notification Create", JSON.stringify(data));
+    logIt("DEBUG", "Notification Create", JSON.stringify(data));
 
   
     let channelId = data.channel_id;
@@ -20,12 +20,8 @@ class NotificationHandler {
     let timeStamp = data.message.timestamp;
   
     let content = '';
-    if (data.message && data.message.content_parsed) {
-      // if message contains just an image then content would be empty.
-      // content = data.message.content_parsed.map((item) => item.content).join(" ");
-      content = data.message.body;
-    }
-  
+    let userAvatarBase64 = '';
+
     // GuildID is not always available.. so we need to find it based on the channelID
     // This allows us to return the guildID the message is in as well as the guildName and determine if its a voice or a text channel
     let guildId;
@@ -54,58 +50,48 @@ class NotificationHandler {
       console.log(`Guild Name: ${guildName}`);
       console.log(`Channel Type: ${channelType}`);
     }
-  
-    // Allows user to open DM and or Text channel based on notification data we have..
-    if (channelType === "voice") {
-      // this.DG.Client.selectVoiceChannel(channelId, {timeout: 5});
-    } else if (channelType === "text") {
-
+    
+    // Doing stuff based on the channel type
+    switch (channelType) {
+      case "voice":
+        console.log("Looks like you just got tagged in a voice channel.. not sure what we can do with that yet..");
+        // this.DG.Client.selectVoiceChannel(channelId, {timeout: 5});
+        break;
       
-      console.log(
-        "TEXT CHANNEL |  Guild: ",
-        guildName,
-        " Author:",
-        userName,
-        "ID:",
-        userId,
-        "Channel ID:",
-        channelId,
-        "Content:",
-        content
-      );
-      // this.DG.Client.selectTextChannel(channelId, {timeout: 5});
-    } else if (channelType === "dm") {
+      case "text":
+        content = data.body;
+        logIt("INFO", "TEXT CHANNEL |  Guild:", guildName, "Author:", userName, "ID:", userId, "Channel ID:", channelId, "Content:", content);
+        // should we make a category for 'new notification' and use it for text notification, 
+        // and then keep the one we have for DMs as such.. sure its a notification but people recognize it as a 'DM' solely..
+        break;
+      
+      case "dm":
+        content = data.message.body;
+        userAvatarBase64 = await imageToBase64(avatarUrl);
+        //// Save this for if we need to captrue the last X dms
+        // this.TPClient.createState("discord_DM_user", "DM: UserName",  userName, "DirectMessage");
+        // this.TPClient.createState("discord_DM_userID", "DM: UserID", userId, "DirectMessage");
+        // this.TPClient.createState("discord_DM_channelID", "DM: ChannelID", channelId, "DirectMessage");
+        // this.TPClient.createState("discord_DM_content", "DM: Content:", content, "DirectMessage");
+        // const userAvatarBase64 = await imageToBase64(avatarUrl);
+        // this.TPClient.createState("discord_DM_avatar", "avatarIcon", userAvatarBase64, "DirectMessage");
+  
+        this.TPClient.stateUpdate("discord_newDM_eventState", "true");
+        this.TPClient.stateUpdate("discord_newDM_eventState", "false");
+        this.TPClient.stateUpdate("discord_DM_user", userName);
+        this.TPClient.stateUpdate("discord_DM_userID", userId);
+        this.TPClient.stateUpdate("discord_DM_channelID", channelId);
+        this.TPClient.stateUpdate("discord_DM_content", content);
+        this.TPClient.stateUpdate("discord_DM_timestamp", timeStamp);
+        this.TPClient.stateUpdate("discord_DM_avatar", userAvatarBase64);
+  
+        logIt("INFO", "DIRECT MESSAGE | Author:", userName, "ID:", userId, "Channel ID:", channelId, "Content:", content);
 
-      // this.TPClient.createState("discord_DM_user", "DM: UserName",  userName, "DirectMessage");
-      // this.TPClient.createState("discord_DM_userID", "DM: UserID", userId, "DirectMessage");
-      // this.TPClient.createState("discord_DM_channelID", "DM: ChannelID", channelId, "DirectMessage");
-      // this.TPClient.createState("discord_DM_content", "DM: Content:", content, "DirectMessage");
-      // const userAvatarBase64 = await imageToBase64(avatarUrl);
-      // this.TPClient.createState("discord_DM_avatar", "avatarIcon", userAvatarBase64, "DirectMessage");
-
-      let userAvatarBase64 = await imageToBase64(avatarUrl);
-      this.TPClient.stateUpdate("discord_newDM_eventState", "true");
-      this.TPClient.stateUpdate("discord_newDM_eventState", "false");
-      this.TPClient.stateUpdate("discord_DM_user", userName);
-      this.TPClient.stateUpdate("discord_DM_userID", userId);
-      this.TPClient.stateUpdate("discord_DM_channelID", channelId);
-      this.TPClient.stateUpdate("discord_DM_content", content);
-      this.TPClient.stateUpdate("discord_DM_timestamp", timeStamp);
-      this.TPClient.stateUpdate("discord_DM_avatar", userAvatarBase64);
-
-
-      console.log(
-        "DIRECT MESSAGE | Author:",
-        userName,
-        "ID:",
-        userId,
-        "Channel ID:",
-        channelId,
-        "Content:",
-        content
-      );
-      // this.DG.Client.selectTextChannel(channelId, {timeout: 5});
+      default:
+        console.log("Unknown channel type");
+        break;
     }
+
   }
 }
 
